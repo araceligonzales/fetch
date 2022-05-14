5b5d0a4ef936e5882371cee3e632a5ac19d19b73
Kelvin Chao <kelvinchao2011@gmail.com>
2022-05-14 00:59:26 -0700

    more handlebars

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,16 +3,40 @@ const { User } = require('../models')
 
 const userdata = [
   {
-    username: 'k',
-    first_name: 'k',
-    last_name: 'c',
-    email: 'kc@s.com',
-    password: 'abc',
-    // location: '',
-    // pet_type: '',
-    // pet_name: '',
-    // pet_age: '',
-    // pet_traits: ''
+    username: 'jacqueline',
+    first_name: 'Jackqueline',
+    last_name: 'S',
+    email: 'hfdskjh',
+    password: '',
+    location: '',
+    pet_type: '',
+    pet_name: '',
+    pet_age: 2,
+    pet_traits: ''
+  },
+  {
+    username: 'rob',
+    first_name: 'Rob',
+    last_name: 'B',
+    email: '',
+    password: '',
+    location: '',
+    pet_type: '',
+    pet_name: '',
+    pet_age: 1,
+    pet_traits: ''
+  },
+  {
+    username: 'alice',
+    first_name: 'Alice',
+    last_name: 'B',
+    email: '',
+    password: '',
+    location: '',
+    pet_type: '',
+    pet_name: '',
+    pet_age: 3,
+    pet_traits: ''
   }
 ]
 
